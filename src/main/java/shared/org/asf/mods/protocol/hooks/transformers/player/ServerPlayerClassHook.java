a3bc6860e1bf3356be5b01e6263da14d3cea8df7
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-08-07 13:38:43 +0200

    Patch ServerPlayerClassHook

--- src/main/java/shared/org/asf/mods/protocol/hooks/transformers/player/ServerPlayerClassHook.java
+++ src/main/java/shared/org/asf/mods/protocol/hooks/transformers/player/ServerPlayerClassHook.java
@@ -2,6 +2,7 @@ package org.asf.mods.protocol.hooks.transformers.player;
 
 import java.security.ProtectionDomain;
 
+import org.asf.cyan.fluid.Fluid;
 import org.asf.cyan.fluid.api.ClassLoadHook;
 import org.asf.cyan.fluid.bytecode.FluidClassPool;
 import org.objectweb.asm.ClassWriter;
@@ -12,10 +13,12 @@ import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.TypeInsnNode;
 
-import modkit.enhanced.player.EnhancedPlayer;
-import net.minecraft.server.level.ServerPlayer;
-
 public class ServerPlayerClassHook extends ClassLoadHook {
+
+	private static final String PerverPlayerDeobf = "net.minecraft.server.level.ServerPlayer";
+	private static final String EnhancedPlayer = " modkit.enhanced.player.EnhancedPlayer";
+	
+	private static String ServerPlayer;
 
 	@Override
 	public String targetPath() {
@@ -24,15 +27,16 @@ public class ServerPlayerClassHook extends ClassLoadHook {
 
 	@Override
 	public void build() {
+		ServerPlayer = Fluid.mapClass(PerverPlayerDeobf);
 	}
 
 	@Override
 	public void apply(ClassNode cc, FluidClassPool cp, ClassLoader loader, Class<?> classBeingRedefined,
 			ProtectionDomain protectionDomain, byte[] classfileBuffer) throws ClassNotFoundException {
 		boolean edited = false;
-		if (cc.superName != null && !cc.name.equals(EnhancedPlayer.class.getTypeName().replace(".", "/"))
-				&& cc.superName.equals(ServerPlayer.class.getTypeName().replace(".", "."))) {
-			cc.superName = EnhancedPlayer.class.getTypeName().replace(".", "/");
+		if (cc.superName != null && !cc.name.equals(EnhancedPlayer.replace(".", "/"))
+				&& cc.superName.equals(ServerPlayer.replace(".", "."))) {
+			cc.superName = EnhancedPlayer.replace(".", "/");
 			edited = true;
 		}
 		if (cc.methods != null) {
@@ -40,17 +44,16 @@ public class ServerPlayerClassHook extends ClassLoadHook {
 				for (AbstractInsnNode nd : mth.instructions) {
 					if (nd instanceof TypeInsnNode) {
 						TypeInsnNode typeNode = (TypeInsnNode) nd;
-						if (typeNode.desc.equals(ServerPlayer.class.getTypeName().replace(".", "/"))) {
-							typeNode.desc = EnhancedPlayer.class.getTypeName().replace(".", "/");
+						if (typeNode.desc.equals(ServerPlayer.replace(".", "/"))) {
+							typeNode.desc = EnhancedPlayer.replace(".", "/");
 							edited = true;
 							if (typeNode.getOpcode() == Opcodes.NEW) {
 								AbstractInsnNode nd2 = nd.getNext();
 								while (nd2 != null) {
 									if (nd2 instanceof MethodInsnNode && nd2.getOpcode() == Opcodes.INVOKESPECIAL
-											&& ((MethodInsnNode) nd2).owner
-													.equals(ServerPlayer.class.getTypeName().replace(".", "/"))) {
+											&& ((MethodInsnNode) nd2).owner.equals(ServerPlayer.replace(".", "/"))) {
 										MethodInsnNode ctor = (MethodInsnNode) nd2;
-										ctor.owner = EnhancedPlayer.class.getTypeName().replace(".", "/");
+										ctor.owner = EnhancedPlayer.replace(".", "/");
 									}
 
 									nd2 = nd2.getNext();
